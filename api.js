095d6b2f3b42f7e1cbda79afd0cc557fc7a8b08a
Louis Grasset <louisgrasset@protonmail.com>
2018-07-29 00:17:32 +0200

    :tada: Initial commit

---
+++ api.js
@@ -0,0 +1,9 @@
+const API_KEY = "yourapikey";
+const CHANNEL_ID = "yourapikey";
+/*
+Create your own project here and get your API key
+https://console.developers.google.com/projectselector/apis/credentials?supportedpurview=project
+
+Then get your youtube channel id here
+https://www.youtube.com/account_advanced
+*/
