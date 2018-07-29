8c64f142056b0c87bac03c2982ea314f7bf7e725
Louis Grasset <louisgrasset@protonmail.com>
2018-07-30 00:47:50 +0200

    :pencil: Change variables order

--- api.js
+++ api.js
@@ -1,9 +1,9 @@
+const CHANNEL_ID = "yourchannelid";
 const API_KEY = "yourapikey";
-const CHANNEL_ID = "yourapikey";
 /*
-Create your own project here and get your API key
-https://console.developers.google.com/projectselector/apis/credentials?supportedpurview=project
-
 Then get your youtube channel id here
 https://www.youtube.com/account_advanced
+
+Create your own project here and get your API key
+https://console.developers.google.com/projectselector/apis/credentials?supportedpurview=project
 */
